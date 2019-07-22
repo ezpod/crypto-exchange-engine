6498cd7f7b236bebff02cd424b07d1218a0860e1
ezpod <13641269882@163.com>
2019-07-22 21:01:22 +0800

    Create order_book.go

---
+++ engine/order_book.go
@@ -0,0 +1,53 @@
+package engine
+
+// OrderBook type
+type OrderBook struct {
+	BuyOrders  []Order
+	SellOrders []Order
+}
+
+// Add a buy order to the order book
+func (book *OrderBook) addBuyOrder(order Order) {
+	n := len(book.BuyOrders)
+	var i int
+	for i := n - 1; i >= 0; i-- {
+		buyOrder := book.BuyOrders[i]
+		if buyOrder.Price < order.Price {
+			break
+		}
+	}
+	if i == n-1 {
+		book.BuyOrders = append(book.BuyOrders, order)
+	} else {
+		copy(book.BuyOrders[i+1:], book.BuyOrders[i:])
+		book.BuyOrders[i] = order
+	}
+}
+
+// Add a sell order to the order book
+func (book *OrderBook) addSellOrder(order Order) {
+	n := len(book.SellOrders)
+	var i int
+	for i := n - 1; i >= 0; i-- {
+		sellOrder := book.SellOrders[i]
+		if sellOrder.Price > order.Price {
+			break
+		}
+	}
+	if i == n-1 {
+		book.SellOrders = append(book.SellOrders, order)
+	} else {
+		copy(book.SellOrders[i+1:], book.SellOrders[i:])
+		book.SellOrders[i] = order
+	}
+}
+
+// Remove a buy order from the order book at a given index
+func (book *OrderBook) removeBuyOrder(index int) {
+	book.BuyOrders = append(book.BuyOrders[:index], book.BuyOrders[index+1:]...)
+}
+
+// Remove a sell order from the order book at a given index
+func (book *OrderBook) removeSellOrder(index int) {
+	book.SellOrders = append(book.SellOrders[:index], book.SellOrders[index+1:]...)
+}
